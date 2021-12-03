b33efaf2a66ff583ba291af5cd615a51d06deaf1
Meghan Reynolds <meghanr6@linux-03.ews.illinois.edu>
2021-12-03 15:23:52 -0600

    feat: added proof of concept to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
+#include "Graph.h"
+#include "Graph.cpp"
 #include <iostream>
+#include <vector>
 
 int main(int argc, char** argv) {
-  std::cout << "hello, world!" << std::endl;
+  const std::vector<std::string> kDataFilePaths = {"data/April2020.csv", "data/April2021.csv", "data/August2020.csv", "data/December2020.csv",
+      "data/February2020.csv", "data/February2021.csv", "data/January2020.csv", "data/January2021.csv", "data/July2020.csv", "data/June2020.csv",
+      "data/March2020.csv", "data/March2021.csv", "data/May2020.csv", "data/November2020.csv", "data/October2020.csv", "data/September2020.csv"};
+
+  Graph graph;
+  std::cout << "creating graph" << std::endl;
+  for (const std::string& kFilePath : kDataFilePaths) {
+    std::cout << "adding data from " << kFilePath << std::endl;
+    graph.addDataFromFile(kFilePath);
+  }
+  std::cout << "graph created successfully" << std::endl;
   return 0;
 }
