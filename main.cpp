f169b360e2908b6516df1dfd5551d8a0ea89c2fc
Meghan Reynolds <meghanr6@linux-05.ews.illinois.edu>
2021-12-06 01:47:01 -0600

    fix: fixed bug in is connected

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "Graph.h"
 #include "Graph.cpp"
+#include "DFS.h"
+#include "DFS.cpp"
 #include <iostream>
 #include <vector>
 
@@ -8,12 +10,14 @@ int main(int argc, char** argv) {
       "data/February2020.csv", "data/February2021.csv", "data/January2020.csv", "data/January2021.csv", "data/July2020.csv", "data/June2020.csv",
       "data/March2020.csv", "data/March2021.csv", "data/May2020.csv", "data/November2020.csv", "data/October2020.csv", "data/September2020.csv"};
 
-  Graph graph;
+  Graph* graph = new Graph();
   std::cout << "creating graph" << std::endl;
   for (const std::string& kFilePath : kDataFilePaths) {
     std::cout << "adding data from " << kFilePath << std::endl;
-    graph.addDataFromFile(kFilePath);
+    graph->addDataFromFile(kFilePath);
   }
   std::cout << "graph created successfully" << std::endl;
+  
+  std::cout << "result is eulerian: " << graph->isEulerian() << std::endl;
   return 0;
 }
